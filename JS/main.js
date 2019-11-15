d24f61173da5ce8f1a518880515d247c697c9ed0
Chrimmy <56279851+Chrimmy@users.noreply.github.com>
2019-11-15 21:00:05 +1300

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,9 @@
-// Animate Smooth Scroll
+// Animate Smooth Scroll for our work
 $('#view-work').on('click', function(){const images = $('#images').position().top;
 
 $('html,body').animate({scrollTop:images},1000);});
+
+// Animate Smooth Scroll for contact us
+$('#contact-us').on('click', function(){const contact = $('#contact').position().top;
+
+$('html,body').animate({scrollTop:contact},1000);});
